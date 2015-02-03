48fffa7fe20e1d6d6d0653831755a6f254af59dd
Lindsay Eisberg <lindsay.eisberg@gmail.com>
2015-02-03 15:23:54 -0500

    styles added. Functions work

--- scripts/popup.js
+++ scripts/popup.js
@@ -8,23 +8,26 @@ $(document).ready(function(){
       baseUrl: 'https://api.github.com/search/repositories',
       searchResults: find,
       pages: "1",
-      perPage: "10"
+      perPage: "10",
+      stars : 'stars'
 
     };
 
     $.ajax({
-      url: config.baseUrl + "?q=" + config.searchResults + "&page=" + config.pages + "&per_page=" + config.perPage,
+      url: config.baseUrl + "?q=" + config.searchResults + "&page=" + config.pages + "&per_page=" + config.perPage + "&sort=" + config.stars,
       type: 'GET',
       dataType: 'JSONP',
       success: function(results) {
         console.log(results);
         results.data.items.forEach(function(item, index, array){
           $('.results').append(
+            '<div class="post">',
             '<h3>' + item.full_name + '</h3>',
-            '<a href="' + item.html_url + '">' +
+            '<a target="_blank" href="' + item.html_url + '">' +
             'Go to Repo' +
             '</a>' +
-            '<p>' + item.description + '</p>'
+            '<p>' + item.description + '</p>',
+            '</div>'
           );
         });
 
